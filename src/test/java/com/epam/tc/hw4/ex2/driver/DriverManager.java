8c16a36a8c1b2d210158656214a87f9cccf99950
Iuliia_Khitsenko <iuliia.ushakova.qa@gmail.com>
2023-01-10 22:19:22 +0200

    HW4 fixing web driver for jenkins

--- src/test/java/com/epam/tc/hw4/ex2/driver/DriverManager.java
+++ src/test/java/com/epam/tc/hw4/ex2/driver/DriverManager.java
@@ -1,14 +1,26 @@
 package com.epam.tc.hw4.ex2.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     private static WebDriver driver;
 
     public static WebDriver setupDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("disable-infobars"); // disabling infobars
+            options.addArguments("--disable-extensions"); // disabling extensions
+            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+            options.addArguments("--no-sandbox"); // Bypass OS security model
+            options.setHeadless(true);
+            options.addArguments("--remote-debugging-port=9222");
+
+            driver = new ChromeDriver(options);
             driver.manage().window().maximize();
             driver.manage().timeouts().getImplicitWaitTimeout();
         }
